cd21e9734d824e9999d6f5e5b7becf909ee5c08c
aidanbush <aidandbush@gmail.com>
2016-11-15 18:24:08 -0700

    Create types.h
    improves performance through the spirit of harambe

---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef
+#define TYPES_H
+
+#include <stdbool.h>
+
+#include "board.h"
+
+typedef int harambe_t;
+typedef uint8_t uharambe8_t;
+typedef void vharambe_t;//:'(
+typedef bool bharambe_t;
+typedef Board Bharambe_t;
+
+#endif //TYPES_H
