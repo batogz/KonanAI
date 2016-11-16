376f919283fd0e939008c5fc40e8eb28635ce47c
aidanbush <aidandbush@gmail.com>
2016-11-15 18:29:46 -0700

    fix types.h

--- types.h
+++ types.h
@@ -3,12 +3,9 @@
 
 #include <stdbool.h>
 
-#include "board.h"
-
 typedef int harambe_t;
 typedef uint8_t uharambe8_t;
 typedef void vharambe_t;//:'(
 typedef bool bharambe_t;
-typedef Board Bharambe_t;
 
 #endif //TYPES_H
